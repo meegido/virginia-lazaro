0abd91ae12cdaa3a572dc641d13545df2233fa1d
Mercedes <meegido@gmail.com>
2021-01-02 13:59:35 +0100

    header init layout

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,7 +3,15 @@ import './Header.css'
 const Header = () => {
   return (
     <header className="App-header">
+      <div className="subtitle">
+        <span>Crítica</span>
+        <span>cultural</span>
+      </div>
       <h1 className="title">VIRGINIA LÁZARO</h1>
+      <div className="subtitle">
+        <span>Investigadora</span>
+        <span>independiente</span>
+      </div>
     </header>
   )
 }
