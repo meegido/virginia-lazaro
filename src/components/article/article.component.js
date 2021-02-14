c902e9de311ed1b781d3b12dc81b148eaedb1e2f
Mercedes Egido <mercedes.egido@creditas.com>
2021-02-14 18:53:51 +0100

    home layout

--- src/components/article/article.component.js
+++ src/components/article/article.component.js
@@ -23,6 +23,7 @@ export const Article = ({
       <div className="article-info">
         <a className="media" href={media_link}>{media_title}</a>
         <p className="date">{publication_date}</p>
+
       </div>
       <div className="article-content">
         <div className="image-container" onMouseEnter={handleApplyFilter}>
