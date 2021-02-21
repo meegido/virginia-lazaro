82a7b3ca84a970d88325731689cf8cf0d77b35d5
Mercedes Egido <mercedes.egido@creditas.com>
2021-02-21 20:26:32 +0100

    added link to home on title

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -25,7 +25,9 @@ export const Header = () => {
           <span>Crítica</span>
           <span>cultural</span>
         </div>
-        <h1 className="title">VIRGINIA LÁZARO</h1>
+        <h1 className="title">
+          <Link to="/">VIRGINIA LÁZARO</Link>
+        </h1>
         <div className="subtitle">
           <span>Investigadora</span>
           <span>independiente</span>
