20e52486c5381369aabeddaf43529d9736d231dd
Mercedes Egido <mercedes.egido@creditas.com>
2021-02-28 10:41:43 +0100

    profile layout

--- src/pages/about/about.page.js
+++ src/pages/about/about.page.js
@@ -17,7 +17,14 @@ export const About = () => {
             <h2 className="metier">Digital image analyst</h2>
           </div>
         </section>
-        
+        <section className="profile-description">
+          <p>Relaciono las problemáticas específicas derivadas de la tecnología digital, el neoliberalismo, la globalización y las imágenes simbólicas. Esta trayectoria crítica, me mantiene en un enfoque constante sobre el impacto de la tecnología digital en los modos de vida y la definición de los sujetos.</p>
+          <div>
+            <h3>Curriculum biography</h3>
+            <p>Soy licenciada en Bellas Artes por la Universidad Complutense de Madrid, donde obtuve la beca de colaboración con el Departamento de Estética e Historia del Arte, y después cursé el Máster en Culturas Visuales e Historia del Arte Contemporáneo en el Museo Reina Sofía.</p>
+          </div>
+        </section >
+        <section className="tips"></section>
       </main>
     </div>
   )
