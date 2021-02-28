3845339853d0a2640c2a587c8e54fc99e5389eac
Sergio Revilla <sergio.revilla@hey.com>
2021-02-28 11:55:27 +0100

    Add language header

--- src/client/ApiClient.js
+++ src/client/ApiClient.js
@@ -1,15 +1,27 @@
 const ENDPOINT = 'https://virginialazaro.herokuapp.com'
 
+const myHeaders = new Headers({
+  'X-Language': sessionStorage.getItem('language'),
+})
+
 export async function fetchCategories() {
   const path = `${ENDPOINT}/categories/`
-  const response = await window.fetch(path)
+  const response = await window.fetch(path, {
+    headers: {
+      'X-Language': sessionStorage.getItem('language'),
+    },
+  })
 
   return await response.json()
 }
 
 export async function fetchCategory(categoryName) {
   const path = `${ENDPOINT}/categories/${categoryName}/`
-  const response = await window.fetch(path)
+  const response = await window.fetch(path, {
+    headers: {
+      'X-Language': sessionStorage.getItem('language'),
+    },
+  })
 
   return await response.json()
 }
