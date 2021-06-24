dc45a492b454cf25b82b929619fe3217ec894e11
Mercedes Egido <mercedes.egido@creditas.com>
2021-06-24 10:19:23 +0200

    changed background color

--- src/pages/about/content.js
+++ src/pages/about/content.js
@@ -2,10 +2,7 @@ export const content = {
   es: {
     metier: 'Analista de imágenes digitales',
     description: {
-      about:
-        'Relaciono las problemáticas específicas derivadas de la tecnología digital, el neoliberalismo, la globalización y las imágenes simbólicas. \n' +
-        '\n' +
-        'Esta trayectoria crítica, me mantiene en un enfoque constante sobre el impacto de la tecnología digital en los modos de vida y la definición de los sujetos.',
+      about: '', 
       curriculumTitle: 'Curriculum',
       curriculumText:
         'Soy licenciada en Bellas Artes por la Universidad Complutense de Madrid, donde obtuve la beca de colaboración con el Departamento de Estética e Historia del Arte, y después cursé el Máster en Culturas Visuales e Historia del Arte Contemporáneo en el Museo Reina Sofía.',
