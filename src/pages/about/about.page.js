db663ba09785458d5cbc47c3d0fd623ed7fdf4c9
Mercedes Egido <mercedes.egido@creditas.com>
2021-07-09 07:26:37 +0200

    about page with contact

--- src/pages/about/about.page.js
+++ src/pages/about/about.page.js
@@ -15,7 +15,6 @@ export const About = () => {
         <section className="profile-container">
           <div className="profile">
             <div className="avatar"></div>
-            <div className="instagram"></div>
           </div>
         </section>
         <section className="profile-description">
@@ -24,8 +23,15 @@ export const About = () => {
             <p className="curriculum-description-text">{content[language].description.metier}</p>
             <p className="curriculum-description-text">{content[language].description.curriculumText}</p>
           </div>
-          <p className="contact">hi@virginialazaro.com</p>
-
+          <div className="contact-container">
+            <h2 className="contact-title">{content[language].description.contact}</h2>
+            <div className="contact">
+              <a className="insta-link" href="https://www.instagram.com/virginia_lazaro_/" target="blank">
+                <img className="insta-icon" alt="Virginia Lázaro instagram profile" src="https://res.cloudinary.com/meegido/image/upload/v1625807058/d2e53ea31ec15e6a8129008563713de5_ciwxuv.png"/>
+              </a>
+              <p className="contact-mail">hi@virginialazaro.com</p>
+            </div>
+          </div>
         </section>
         <section className="tips"></section>
       </main>
--- src/pages/about/content.js
+++ src/pages/about/content.js
@@ -3,7 +3,8 @@ export const content = {
     description: {
       about: 'Virginia Lázaro es crítica cultural e investigadora afincada en Londres. Su campo de análisis son las imágenes, ocupen el medio que ocupen,  y la relación entre éstas y la construcción de lo social. Tiene un especial interés en aquellas que nos apelan de tal manera que llegan a generar reacciones y respuestas, y en cómo esto se manifiesta en la esfera digital.', 
       metier: 'En la actualidad se encuentra cursando estudios de doctorado en la Universidad de Goldsmiths en Londres, con una investigación que versa sobre las relaciones entre la presente radicalización de la población, el gesto iconoclasta, y los cambios sufridos por las imágenes en su devenir digital. ',
-      curriculumText: 'Es licenciada en Bellas Artes por la Universidad Complutense de Madrid, Master en Historia del Arte Contemporáneo y Cultura Visual en el Museo de Arte Contemporáneo Reina Sofía, con especialización en Crítica de Arte. Como divulgadora y crítica cultural ha publicado en medios como El salto, Political Critique o A*Desk.'
+      curriculumText: 'Es licenciada en Bellas Artes por la Universidad Complutense de Madrid, Master en Historia del Arte Contemporáneo y Cultura Visual en el Museo de Arte Contemporáneo Reina Sofía, con especialización en Crítica de Arte. Como divulgadora y crítica cultural ha publicado en medios como El salto, Political Critique o A*Desk.',
+      contact: 'Contacto'      
     },
   },
   en: {
@@ -11,7 +12,8 @@ export const content = {
       about:
         'Virginia Lázaro is a cultural critic and researcher based in London. Her field of analysis is images, whatever their medium, and the relationship between them and the construction of the social. She is particularly interested in images that appeal to us in such a way that they generate reactions and responses, and how this manifests in the digital sphere.',
       metier: 'She is currently a PhD student at Goldsmiths University in London, where she is researching the relationship between the present radicalisation of the population, the iconoclastic gesture, and the changes undergone by images in their becoming digital.',
-      curriculumText: 'She holds a degree in Fine Arts from the Universidad Complutense de Madrid, a Master degree in Contemporary Art History and Visual Culture from the Museo de Arte Contemporáneo Reina Sofía, with a specialisation in Art Criticism. As a cultural critic she has published in media such as El salto, Political Critique and A*Desk.'
+      curriculumText: 'She holds a degree in Fine Arts from the Universidad Complutense de Madrid, a Master degree in Contemporary Art History and Visual Culture from the Museo de Arte Contemporáneo Reina Sofía, with a specialisation in Art Criticism. As a cultural critic she has published in media such as El salto, Political Critique and A*Desk.',
+      contact: 'Contact'
     },
   },
 }
