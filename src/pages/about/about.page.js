cbaf7e5d6a854dc50b231cf97e96531ce534e5cc
Mercedes Egido <mercedes.egido@creditas.com>
2021-07-09 07:00:40 +0200

    fixed about overlap and change text

--- src/pages/about/about.page.js
+++ src/pages/about/about.page.js
@@ -16,18 +16,16 @@ export const About = () => {
           <div className="profile">
             <div className="avatar"></div>
             <div className="instagram"></div>
-            <p className="contact">hi@virginialazaro.com</p>
-          </div>
-          <div className="profile-data">
-            <h2 className="metier">{content[language].metier}</h2>
           </div>
         </section>
         <section className="profile-description">
           <p className="metier-description">{content[language].description.about}</p>
           <div className="curriculum-description">
-            <h3 className="curriculum-description-title">{content[language].description.curriculumTitle}</h3>
+            <p className="curriculum-description-text">{content[language].description.metier}</p>
             <p className="curriculum-description-text">{content[language].description.curriculumText}</p>
           </div>
+          <p className="contact">hi@virginialazaro.com</p>
+
         </section>
         <section className="tips"></section>
       </main>
--- src/pages/about/content.js
+++ src/pages/about/content.js
@@ -1,23 +1,17 @@
 export const content = {
   es: {
-    metier: 'Analista de imágenes digitales',
     description: {
-      about: '', 
-      curriculumTitle: 'Curriculum',
-      curriculumText:
-        'Soy licenciada en Bellas Artes por la Universidad Complutense de Madrid, donde obtuve la beca de colaboración con el Departamento de Estética e Historia del Arte, y después cursé el Máster en Culturas Visuales e Historia del Arte Contemporáneo en el Museo Reina Sofía.',
+      about: 'Virginia Lázaro es crítica cultural e investigadora afincada en Londres. Su campo de análisis son las imágenes, ocupen el medio que ocupen,  y la relación entre éstas y la construcción de lo social. Tiene un especial interés en aquellas que nos apelan de tal manera que llegan a generar reacciones y respuestas, y en cómo esto se manifiesta en la esfera digital.', 
+      metier: 'En la actualidad se encuentra cursando estudios de doctorado en la Universidad de Goldsmiths en Londres, con una investigación que versa sobre las relaciones entre la presente radicalización de la población, el gesto iconoclasta, y los cambios sufridos por las imágenes en su devenir digital. ',
+      curriculumText: 'Es licenciada en Bellas Artes por la Universidad Complutense de Madrid, Master en Historia del Arte Contemporáneo y Cultura Visual en el Museo de Arte Contemporáneo Reina Sofía, con especialización en Crítica de Arte. Como divulgadora y crítica cultural ha publicado en medios como El salto, Political Critique o A*Desk.'
     },
   },
   en: {
-    metier: 'Digital image analyst',
     description: {
       about:
-        'I relate the specific problems derived from digital technology, neoliberalism, globalization and symbolic images. \n' +
-        '\n' +
-        'This critical trajectory keeps me in a constant focus on the impact of digital technology on the ways of life and the definition of subjects.',
-      curriculumTitle: 'Curriculum biography',
-      curriculumText:
-        'I have a degree in Fine Arts from the Complutense University of Madrid, where I obtained the scholarship for collaboration with the Department of Aesthetics and Art History, and then I studied a Master in Visual Cultures and History of Contemporary Art at the Reina Sofia Museum.',
+        'Virginia Lázaro is a cultural critic and researcher based in London. Her field of analysis is images, whatever their medium, and the relationship between them and the construction of the social. She is particularly interested in images that appeal to us in such a way that they generate reactions and responses, and how this manifests in the digital sphere.',
+      metier: 'She is currently a PhD student at Goldsmiths University in London, where she is researching the relationship between the present radicalisation of the population, the iconoclastic gesture, and the changes undergone by images in their becoming digital.',
+      curriculumText: 'She holds a degree in Fine Arts from the Universidad Complutense de Madrid, a Master degree in Contemporary Art History and Visual Culture from the Museo de Arte Contemporáneo Reina Sofía, with a specialisation in Art Criticism. As a cultural critic she has published in media such as El salto, Political Critique and A*Desk.'
     },
   },
 }
